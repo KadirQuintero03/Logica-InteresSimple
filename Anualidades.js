44add27886e6b270d3d94164cfb442a9d2bb9187
KadirQuintero03 <kadireq1928@gmail.com>
2024-10-02 18:38:06 -0500

    Anualidades anticipada lista

--- Anualidades.js
+++ Anualidades.js
@@ -1,8 +1,8 @@
-let VP = 50000; //Capital o valor presente de una anualidad.
-let VF = 0; //Valor futuro o monto.
-let R = 5000; // Anualidad, cuota o renta periódica.
+let VP = 0; //Capital o valor presente de una anualidad.
+let VF = 20000; //Valor futuro o monto.
+let R = 4000; // Anualidad, cuota o renta periódica.
 let N = 0 * 1; //Tiempo, periodo o plazo de anualidad.
-let I = (8 / 100 / 1).toFixed(4); //Tasa de interés efectiva periódica.
+let I = (3 / 100 / 1).toFixed(4); //Tasa de interés efectiva periódica.
 
 /* ********** Anualidades vencidas **********
 // console.log(AV_renta_teniendo_montoFinal(VF, I, N))
@@ -53,22 +53,24 @@ function AV_periodo_teniendo_valorPresente(VP, I, R) {
 
 //********** Anualidades diferida vencida **********
 
-//********** Anualidades Anticipada **********
-// console.log(AA_renta_teniendo_montoFinal())
-// console.log(AA_periodo_teniendo_montoFinal())
+/* ********** Anualidades Anticipada **********
+// console.log(AA_renta_teniendo_montoFinal(VF, I, N))
+// console.log(AA_periodo_teniendo_montoFinal(VF, I, R))
 // console.log(AA_calcular_valorPresente(R, I, N))
 // console.log(AA_calcular_valorFuturo(R, I, N))
 // console.log(AA_renta_teniendo_valorPresente(VP, I, N))
-console.log(AA_periodo_teniendo_valorPresente(VP, I, R))
+// console.log(AA_periodo_teniendo_valorPresente(VP, I, R))
 
-//Hallar renta cuando tenemos el monto final.
-function AA_renta_teniendo_montoFinal() {
-
+//Hallar renta cuando tenemos el monto final.✅
+function AA_renta_teniendo_montoFinal(VF, I, N) {
+    R = parseInt(VF) / ((Math.pow(1 + parseFloat(I), parseInt(N)) - 1) / parseFloat(I) * (1 + parseFloat(I)))
+    return console.log("El valor de la renta teniendo el monto final es de: ", R.toFixed(2))
 }
 
-//Hallar numero de periodos cuando tenemos monto final.
-function AA_periodo_teniendo_montoFinal() {
-
+//Hallar numero de periodos cuando tenemos monto final.✅
+function AA_periodo_teniendo_montoFinal(VF, I, R) {
+    N = Math.log(((parseInt(VF) * parseFloat(I)) / (parseInt(R) * (1 + parseFloat(I)))) + 1) / Math.log(1 + parseFloat(I))
+    return console.log("El numero de periodos teniendo el monto final es de: ", N.toFixed(2))
 }
 
 //Formula para calcular valor presente.✅
@@ -90,10 +92,8 @@ function AA_renta_teniendo_valorPresente(VP, I, N) {
     return console.log("El valor de la renta teniendo el valor presente es de: ", R.toFixed(2))
 }
 
-//Hallar numero de periodos cuando tenemos valor presente. ❌
+//Hallar numero de periodos cuando tenemos valor presente.✅ 
 function AA_periodo_teniendo_valorPresente(VP, I, R) {
-    let aux = 1 - (parseInt(VP) * parseFloat(I)) / parseInt(R) + parseFloat(I);
-
-    let N = 1 - Math.log(aux) / Math.log(1 + parseFloat(I));
+    let N = 1 - Math.log(1 - (parseInt(VP) * parseFloat(I)) / parseInt(R) + parseFloat(I)) / Math.log(1 + parseFloat(I));
     return console.log("El numero de periodos teniendo el valor presente es de: ", N)
-}
+}*/
