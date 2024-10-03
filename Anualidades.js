8084aad958c3af37cf54a9d2601306b76b876019
KadirQuintero03 <kadireq1928@gmail.com>
2024-10-02 22:03:24 -0500

    Anualidades finalizado

--- Anualidades.js
+++ Anualidades.js
@@ -1,8 +1,9 @@
 let VP = 0; //Capital o valor presente de una anualidad.
-let VF = 20000; //Valor futuro o monto.
-let R = 4000; // Anualidad, cuota o renta periódica.
+let VF = 0; //Valor futuro o monto.
+let R = 0; // Anualidad, cuota o renta periódica.
 let N = 0 * 1; //Tiempo, periodo o plazo de anualidad.
-let I = (3 / 100 / 1).toFixed(4); //Tasa de interés efectiva periódica.
+let Y = 0 * 1; //Periodo de gracia.
+let I = (0 / 100 / 1).toFixed(4); //Tasa de interés efectiva periódica.
 
 /* ********** Anualidades vencidas **********
 // console.log(AV_renta_teniendo_montoFinal(VF, I, N))
@@ -49,9 +50,65 @@ function AV_periodo_teniendo_valorPresente(VP, I, R) {
     return console.log("El valor del numero de periodos teniendo el valor presente es de: ", N.toFixed(2))
 }*/
 
-//********** Anualidades diferida anticipada **********
+/* ********** Anualidades diferida anticipada **********
+// console.log(ADA_calcular_capitalInicial(R, I, N, Y))
+// console.log(ADA_calcular_renta(VP, I, N, Y))
+console.log(ADA_calcular_periodoGracia(VP, I, N, R))
+// console.log(ADA_calcular_tiempo(VP, I, R, Y))
 
-//********** Anualidades diferida vencida **********
+//Hallar el capital inicial.✅
+function ADA_calcular_capitalInicial(R, I, N, Y) {
+    VP = parseInt(R) * ((1 - Math.pow(1 + parseFloat(I), -parseInt(N))) / (Math.pow(1 + parseFloat(I), parseInt(Y - 1)) * parseFloat(I)))
+    return console.log("El capital inicial de la ADA es de: ", VP.toFixed(2))
+}
+
+//Hallar la renta.✅
+function ADA_calcular_renta(VP, I, N, Y) {
+    R = ((parseInt(VP) * parseFloat(I)) / (1 - Math.pow(1 + parseFloat(I), -parseInt(N)))) * (Math.pow(1 + parseFloat(I), parseInt(Y - 1)) / 1)
+    return console.log("El valor de la renta de la ADA es de: ", R.toFixed(2))
+}
+
+//Hallar el periodo de gracia.✅
+function ADA_calcular_periodoGracia(VP, I, N, R) {
+    Y = (Math.log(parseInt(R)) * (1 + parseFloat(I)) * (1 - Math.pow(1 + parseFloat(I), -parseInt(N))) - Math.log(parseInt(VP) * parseFloat(I))) / Math.log(1 + parseFloat(I))
+    console.log("El periodo de gracia de la ADA es de: ", Y.toFixed(2))
+}
+
+//Hallar el tiempo.✅
+function ADA_calcular_tiempo(VP, I, R, Y) {
+    N = (Math.log(parseInt(R)) - Math.log(parseInt(R) - parseInt(VP) * (Math.pow(1 + parseFloat(I), parseInt(Y - 1)) * parseFloat(I)))) / Math.log(1 + parseFloat(I))
+    console.log("El tiempo calculado en el ADA es de: ", N.toFixed(2))
+}*/
+
+/* ********** Anualidades diferida vencida **********
+// console.log(ADV_calcular_capitalInicial(R, I, N, Y))
+// console.log(ADV_calcular_renta(VP, I, N, Y))
+// console.log(ADV_calcular_periodoGracia(VP, I, N, R))
+// console.log(ADV_calcular_tiempo(VP, I, R, Y))
+
+//Hallar el capital inicial.✅
+function ADV_calcular_capitalInicial(R, I, N, Y) {
+    VP = parseInt(R) * ((1 - Math.pow(1 + parseFloat(I), -parseInt(N))) / (Math.pow(1 + parseFloat(I), parseInt(Y)) * parseFloat(I)))
+    return console.log("El capital inicial de la ADV es de: ", VP.toFixed(2))
+}
+
+//Hallar la renta.✅
+function ADV_calcular_renta(VP, I, N, Y) {
+    R = ((parseInt(VP) * parseFloat(I)) / (1 - Math.pow(1 + parseFloat(I), -parseInt(N)))) * Math.pow(1 + parseFloat(I), parseInt(Y))
+    return console.log("El valor de la renta de la ADV es de: ", R.toFixed(2))
+}
+
+//Hallar el periodo de gracia.✅
+function ADV_calcular_periodoGracia(VP, I, N, R) {
+    Y = ((Math.log(parseInt(R)) + Math.log(1 - Math.pow(1 + parseFloat(I), -parseInt(N)))) - Math.log(parseInt(VP) * parseFloat(I))) / Math.log(1 + parseFloat(I))
+    console.log("El periodo de gracia de la ADV es de: ", Y.toFixed(2))
+}
+
+//Hallar el tiempo.✅
+function ADV_calcular_tiempo(VP, I, R, Y) {
+    N = (Math.log(parseInt(R)) - Math.log(parseInt(R) - parseInt(VP) * (Math.pow(1 + parseFloat(I), parseInt(Y)) * parseFloat(I)))) / Math.log(1 + parseFloat(I))
+    console.log("El tiempo calculado en el ADV es de: ", N.toFixed(2))
+}*/
 
 /* ********** Anualidades Anticipada **********
 // console.log(AA_renta_teniendo_montoFinal(VF, I, N))
@@ -87,7 +144,6 @@ function AA_calcular_valorFuturo(R, I, N) {
 
 //Hallar renta cuando tenemos el valor presente.✅
 function AA_renta_teniendo_valorPresente(VP, I, N) {
-    console.log(VP, I, N)
     R = parseInt(VP) * parseFloat(I) / ((1 + parseFloat(I)) * (1 - Math.pow(1 + parseFloat(I), -parseInt(N))))
     return console.log("El valor de la renta teniendo el valor presente es de: ", R.toFixed(2))
 }
